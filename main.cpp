db528b5501a859dea88127e91151800db1822bca
竹之内暖智 <k024g0069@g.neec.ac.jp>
2025-07-04 10:23:34 +0900

    03_01完了

--- main.cpp
+++ main.cpp
@@ -1,35 +1,51 @@
 #include <Novice.h>
 #include "Math.h"
-#include "Collision.h"
-#include "DebugUI.h"
 #include "Camera.h"
-#include "Sphere.h"
-#include "Grid.h"
 #include "RenderContext.h"
+#include "Grid.h"
 #include <imgui.h>
+#include <string>
 
-const char kWindowTitle[] = "LC1A_15_タケノウチ_ナチ_タイトル";
+const char kWindowTitle[] = "Hierarchy Scene";
+
+struct Object {
+    Transform transform;
+    Object* parent = nullptr;
+
+    Matrix4x4 GetWorldMatrix() const {
+        if (parent) {
+            return Multiply(transform.GetMatrix(), parent->GetWorldMatrix());
+        }
+        return transform.GetMatrix();
+    }
+};
+
+void DrawMarker(const Vector3& worldPos, const Matrix4x4& view, const Matrix4x4& proj, const Matrix4x4& vp, uint32_t color) {
+    Matrix4x4 mvp = Multiply(Multiply(view, proj), vp);
+    Vector3 screen = TransformVector(worldPos, mvp);
+    Novice::DrawEllipse((int)screen.x, (int)screen.y, 5, 5, 0.0f, color, kFillModeSolid);
+}
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Novice::Initialize(kWindowTitle, 1280, 720);
-    char keys[256] = { 0 };
-    char preKeys[256] = { 0 };
+    char keys[256] = {}, preKeys[256] = {};
 
-    Grid grid;
-    Sphere sphereA;
     Camera camera;
     RenderContext renderContext;
+    Grid grid;
+
+    Object root, childA, childB;
+    root.transform.position = { 0, 0, 0 };
+    childA.transform.position = { 0, 1, 0 };
+    childB.transform.position = { 0, 1, 0 };
+
+    childA.parent = &root;
+    childB.parent = &childA;
 
     auto Update = [&]() {
         camera.Update();
-        grid.Update();
-        sphereA.Update();
         renderContext.Update(camera, 1280.0f, 720.0f);
-    };
-
-    //球の位置を初期化
-    sphereA.transform.position = { 0,0,5 };
-    sphereA.radius = 1.0f;
+        };
 
     while (Novice::ProcessMessage() == 0) {
         Novice::BeginFrame();
@@ -38,13 +54,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
         Update();
 
-        sphereA.isColliding = SphereVsPlane(sphereA.transform.position, sphereA.radius, grid.planePoint, grid.planeNormal);
+        Matrix4x4 view = renderContext.view;
+        Matrix4x4 proj = renderContext.proj;
+        Matrix4x4 vp = renderContext.vp;
 
-        grid.Draw(renderContext.view, renderContext.proj, renderContext.vp);
-        sphereA.Draw(renderContext.view, renderContext.proj, renderContext.vp);
+        grid.Draw(view, proj, vp);
 
-        camera.ShowImGuiUI();
-        sphereA.ShowImGuiUI();
+        Vector3 posRoot = { root.GetWorldMatrix().m[3][0], root.GetWorldMatrix().m[3][1], root.GetWorldMatrix().m[3][2] };
+        Vector3 posChildA = { childA.GetWorldMatrix().m[3][0], childA.GetWorldMatrix().m[3][1], childA.GetWorldMatrix().m[3][2] };
+        Vector3 posChildB = { childB.GetWorldMatrix().m[3][0], childB.GetWorldMatrix().m[3][1], childB.GetWorldMatrix().m[3][2] };
+
+        DrawMarker(posRoot, view, proj, vp, 0xFFFF0000);
+        DrawMarker(posChildA, view, proj, vp, 0x00FF00FF);
+        DrawMarker(posChildB, view, proj, vp, 0x0000FFFF);
+
+        ImGui::Begin("Root");
+        ImGui::DragFloat3("Position", &root.transform.position.x, 0.01f);
+        ImGui::End();
+
+        ImGui::Begin("Child A");
+        ImGui::DragFloat3("Position", &childA.transform.position.x, 0.01f);
+        ImGui::End();
+
+        ImGui::Begin("Child B");
+        ImGui::DragFloat3("Position", &childB.transform.position.x, 0.01f);
+        ImGui::End();
 
         Novice::EndFrame();
         if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) break;
